8c2b8aae45ee6488d6b88a0dca8cebb42fc35594
Dmitry Klevakov <dklevakov@gmail.com>
2022-10-23 17:42:58 +0500

    Добавлены АПИ

--- conf/settings.py
+++ conf/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -29,7 +30,7 @@ SECRET_KEY = 't6(=f(==s8%00v^efidz+7m!oc%9l-z)-&+=pn&sfnpe=es*f+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
@@ -41,9 +42,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'organizations_directory.apps.OrganizationsDirectoryConfig',
+    'rest_framework',
+    'django_filters',
+    'rest_framework_simplejwt',
+    'drf_yasg',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -109,6 +120,32 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+    'JTI_CLAIM': 'jti',
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=30),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -128,3 +165,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {'basic': {'type': 'basic'}},
+}
--- organizations_directory/views.py
+++ organizations_directory/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.db.models import Max, Min
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, viewsets
 
-# Create your views here.
+from organizations_directory.models import Organization, Product
+from organizations_directory.serializers import OrganizationSerializer, \
+    ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    Обработка действий с продуктами/услугами.
+    """
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    http_method_names = ['get', 'post']
+
+
+class OrganizationViewSet(viewsets.ModelViewSet):
+    """
+    Возвращает список заведений и детальную информацию по заведению.
+    """
+
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
+    http_method_names = ['get']
+
+
+class OrganizationByDistrictViewSet(viewsets.ModelViewSet):
+    """
+    Возвращает список заведений с фильтрацией по району города,
+    минимальной/максимальной цене, категории товара и посиком по названию товара.
+    """
+
+    queryset = Organization.objects.all()
+    serializer_class = OrganizationSerializer
+    http_method_names = ['get']
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['product__category__name']
+    search_fields = ['product__name']
+
+    def get_queryset(self):
+        self.queryset = self.queryset.filter(districts=self.kwargs.pop('pk'))
+        min_price = self.request.query_params.get('min_price')
+        if min_price:
+            self.queryset = self.queryset.annotate(
+                min_price=Min('offer__price')
+            ).filter(min_price=min_price)
+
+        max_price = self.request.query_params.get('max_price')
+        if max_price:
+            self.queryset = self.queryset.annotate(
+                max_price=Max('offer__price')
+            ).filter(max_price=max_price)
+        return self.queryset
